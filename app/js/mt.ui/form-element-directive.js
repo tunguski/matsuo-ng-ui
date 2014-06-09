d30eb759995d36f5836cf07d17af6b6c54746d44
marek <marek.romanowski@gmail.com>
2014-06-09 21:18:02 +0200

    Base for form generation.

---
+++ app/js/mt.ui/form-element-directive.js
@@ -0,0 +1,61 @@
+'use strict';
+
+angular.module('mt.ui')
+    .directive('mtFormPart', function () {
+      return {
+        replace: true,
+        transclude: true,
+        template: '<div ng-transclude></div>',
+        scope: {
+          entityClass: '@mtFormPart',
+          entityName: '@mtEntityName',
+          inline: '@mtInline',
+          singleField: '@mtSingleField',
+          cssClasses: '@mtCssClasses'
+        },
+        controller: function ($scope) {
+          this.baseParams = function () {
+            return {
+              entityClass: $scope.entityClass,
+              entityName: $scope.entityName || 'entity',
+              inline: $scope.inline,
+              singleField: $scope.singleField,
+              cssClasses: $scope.cssClasses
+            };
+          }
+        }
+      };
+    })
+
+    .directive('mtFormField', function () {
+      return {
+        require: '^mtFormPart',
+        replace: true,
+        template: '<ng-include src="fieldUrl()" />',
+        scope: {
+          fieldName: '@mtFormField',
+          htmlName: '@mtHtmlName',
+          cssClasses: '@mtCssClasses'
+        },
+        link: function (scope, iElement, iAttrs, ctrl) {
+          scope.fieldUrl = function () {
+            var params = ctrl.baseParams();
+            params.fieldName = scope.fieldName;
+            params.htmlName = scope.htmlName;
+            if (scope.cssClasses) {
+              params.cssClasses = (params.cssClasses ? params.cssClasses + ' ' : '') + scope.cssClasses;
+            }
+
+            angular.forEach(_.filter(_.keys(iAttrs.$attr), function (key) { return key.indexOf('mtf') == 0; }),
+                function (key) {
+                  params[key] = iAttrs[key];
+                });
+
+
+            return "/field/url.html?" + _.toUrlParams(params);
+          };
+
+        }
+      };
+    })
+;
---
+++ test/js/unit/test_formElement.js
@@ -0,0 +1,80 @@
+describe('Form element -', function () {
+
+
+  function checkGeneratedGET(htmlPart, expectedGET, expectedGET2) {
+    http.expectGET(expectedGET).respond('<div class="field">field</div>');
+    if (expectedGET2) {
+      http.expectGET(expectedGET2).respond('<div class="field">field</div>');
+    }
+    var template = compile(htmlPart)(scope);
+    rootScope.$digest();
+    http.flush();
+    console.log(template.html());
+    expect(template.html()).toContain('<div class="field ng-scope">field</div>');
+    return template;
+  }
+
+
+  it('basic entity field', function () {
+    checkGeneratedGET('<div mt-form-part="Person"><div mt-form-field="firstName" /></div>',
+        '/field/url.html?entityClass=Person&entityName=entity&fieldName=firstName&');
+  });
+
+
+  it('list of basic fields', function () {
+    var template = checkGeneratedGET(
+        '<div mt-form-part="Person"><div mt-form-field="firstName" /><div mt-form-field="lastName" /></div>',
+        '/field/url.html?entityClass=Person&entityName=entity&fieldName=firstName&',
+        '/field/url.html?entityClass=Person&entityName=entity&fieldName=lastName&');
+
+    expect(template.html().split("field ng-scope").length).toBe(3);
+  });
+
+
+  it('custom entity name', function () {
+    checkGeneratedGET('<div mt-form-part="Tool" mt-entity-name="person"><div mt-form-field="name" /></div>',
+        '/field/url.html?entityClass=Tool&entityName=person&fieldName=name&');
+  });
+
+
+  it('custom html field name', function () {
+    checkGeneratedGET('<div mt-form-part="Company"><div mt-form-field="idOwner" mt-html-name="owner.id" /></div>',
+        '/field/url.html?entityClass=Company&entityName=entity&fieldName=idOwner&htmlName=owner.id&');
+  });
+
+
+  it('with attributes', function () {
+    checkGeneratedGET('<div mt-form-part="Person"><div mt-form-field="firstName" mtf-ng-disabled="true" mtf-custom-attr="xxxy"></div></div>',
+        '/field/url.html?entityClass=Person&entityName=entity&fieldName=firstName&mtfNgDisabled=true&mtfCustomAttr=xxxy&');
+  });
+
+
+  it('inline', function () {
+    checkGeneratedGET('<div mt-form-part="Person" mt-inline="true"><div mt-form-field="firstName"></div></div>',
+        '/field/url.html?entityClass=Person&entityName=entity&inline=true&fieldName=firstName&');
+  });
+
+
+  it('single field', function () {
+    checkGeneratedGET('<div mt-form-part="Person" mt-single-field="true"><div mt-form-field="firstName"></div></div>',
+        '/field/url.html?entityClass=Person&entityName=entity&singleField=true&fieldName=firstName&');
+  });
+
+  describe('css classes -', function () {
+    it('with css classes', function () {
+      checkGeneratedGET('<div mt-form-part="Person" mt-css-classes="yellow big"><div mt-form-field="firstName"></div></div>',
+          '/field/url.html?entityClass=Person&entityName=entity&cssClasses=yellow big&fieldName=firstName&');
+    });
+
+    it('with css classes', function () {
+      checkGeneratedGET('<div mt-form-part="Person"><div mt-form-field="firstName" mt-css-classes="red small"></div></div>',
+          '/field/url.html?entityClass=Person&entityName=entity&cssClasses=red small&fieldName=firstName&');
+    });
+
+    it('with css classes', function () {
+      checkGeneratedGET('<div mt-form-part="Person" mt-css-classes="big"><div mt-form-field="firstName" mt-css-classes="red"></div></div>',
+          '/field/url.html?entityClass=Person&entityName=entity&cssClasses=big red&fieldName=firstName&');
+    });
+  });
+});
+
