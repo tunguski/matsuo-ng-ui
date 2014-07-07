5221cfdc8c5810a3f5b375c6c322c9f177c5c356
Marek Romanowski <marek.romanowski@gmail.com>
2014-07-07 14:18:13 +0200

    Remove isolated scope for form-field, because it conflicted with ui-select2.

--- app/scripts/mt.ui/directives/form-element.js
+++ app/scripts/mt.ui/directives/form-element.js
@@ -35,31 +35,29 @@ angular.module('mt.ui')
         require: '^mtFormPart',
         replace: true,
         template: '<div ng-bind-html="fieldBody"></div>',
-//        template: '<div></div>',
-        scope: {
-          fieldName: '@mtFormField',
-          htmlName: '@mtHtmlName',
-          cssClasses: '@mtCssClasses'
-        },
         link: function (scope, iElement, iAttrs, ctrl) {
-          scope.fieldUrl = function () {
+          function fieldUrl() {
+            // get basic params from outer mt-form-part
             var params = ctrl.baseParams();
-            params.fieldName = scope.fieldName;
-            params.htmlName = scope.htmlName;
-            if (scope.cssClasses) {
-              params.cssClasses = (params.cssClasses ? params.cssClasses + ' ' : '') + scope.cssClasses;
+            // basic parameters
+            params.fieldName = iAttrs.mtFormField;
+            params.htmlName = iAttrs.mtHtmlName;
+            if (iAttrs.mtCssClasses) {
+              params.cssClasses = (params.cssClasses ? params.cssClasses + ' ' : '') + iAttrs.mtCssClasses;
             }
 
+            // rewrite all attributes which name starts from mtf to params
             angular.forEach(_.filter(_.keys(iAttrs.$attr), function (key) { return key.indexOf('mtf') === 0; }),
                 function (key) {
                   params[key] = iAttrs[key];
                 });
 
-
+            // add url prefix
             return mtFormConfig.bootstrapUrlBase + '?' + _.toUrlParams(params);
-          };
+          }
 
-          $http.get(scope.fieldUrl()).success(function (html) {
+          $http.get(fieldUrl()).success(function (html) {
+            // cleanup of template
             html = html.trim();
             if (html.indexOf('"') == 0) {
               html = html.substr(1, html.length - 2).trim();
