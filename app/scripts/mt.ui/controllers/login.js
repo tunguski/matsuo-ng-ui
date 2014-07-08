74e520629bf0fcee9cde3bc607137787c8c20225
Marek Romanowski <marek.romanowski@gmail.com>
2014-07-08 15:29:47 +0200

    Fix broken ctrl.

--- app/scripts/mt.ui/controllers/login.js
+++ app/scripts/mt.ui/controllers/login.js
@@ -9,15 +9,6 @@ angular.module('mt.ui')
      * Controller of the mt.ui
      */
     .controller('LoginCtrl', function ($scope, $dialog) {
-      $scope.showLoginView = function () {
-        $scope.loginView = '/views/login/login.html';
-      };
-      $scope.showLoginView();
-
-      $scope.showCreateAccountView = function () {
-        $scope.loginView = '/views/login/createAccount.html';
-      };
-
       $scope.remindPassword = $dialog.simpleDialog('/views/login/remindPasswordModal.html', 'RemindPasswordDialogCtrl');
       $scope.createAccount = $dialog.simpleDialog('/views/login/createAccountModal.html', 'CreateAccountDialogCtrl');
     })
