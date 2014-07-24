53fdf74d7d6b66ab72bfff2ed7c71150885d54b4
Marek Romanowski <marek.romanowski@gmail.com>
2014-07-24 16:03:52 +0200

    Change date filters configuration.

--- app/scripts/mt.ui/ui.js
+++ app/scripts/mt.ui/ui.js
@@ -1,184 +1,200 @@
 'use strict';
 
-angular.module('mt.ui', ['ui.bootstrap', 'ui.select2', 'mt.route', 'mt.resource', 'pascalprecht.translate'])
-    .config(function($routeProvider, $httpProvider, $sceProvider) {
-      // Completely disable SCE.  For demonstration purposes only!
-      // Do not use in new projects.
-      $sceProvider.enabled(false);
+(function () {
+  var mt_ui = angular.module('mt.ui', ['ui.bootstrap', 'ui.select2', 'mt.route', 'mt.resource', 'pascalprecht.translate'])
+      .config(function($routeProvider, $httpProvider, $sceProvider) {
+        // Completely disable SCE.  For demonstration purposes only!
+        // Do not use in new projects.
+        $sceProvider.enabled(false);
 
-      $httpProvider.defaults.useXDomain = true;
-      $httpProvider.defaults.withCredentials = true;
-    })
-
-
-    .run(function ($rootScope, $injector) {
-      $rootScope.getService = function (name) {
-        return $injector.get(name);
-      };
-      $rootScope.hasService = function (name) {
-        return $injector.has(name);
-      };
-    })
-
-    .filter('formatDate', function() { return function(input) { return input ? parseDate(input).format('YYYY-MM-DD') : ''; };})
-    .filter('formatTime', function() { return function(input) { return input ? parseDate(input).format('HH:mm') : ''; };})
-    .filter('formatDateTime', function() { return function(input) { return input ? parseDate(input).format('YYYY-MM-DD HH:mm') : ''; };})
-    .filter('formatDayMoment', function() { return function(input) { return input ? parseDate(input).format('ddd HH:mm') : ''; };})
-    .filter('weekdayLabel', function() { return function(input) { return input ? parseDate(input).format('dddd') : ''; };})
+        $httpProvider.defaults.useXDomain = true;
+        $httpProvider.defaults.withCredentials = true;
+      })
 
 
-    .filter('addressPresenter', function() {
-      return function (address) {
-        if (!address) {
-          return;
-        }
-        var result = '';
-
-        if (address.zipCode) {
-          result = result + address.zipCode + ' ';
-        }
-        result = result + address.town + '<br/>';
-        result = result + address.street;
-        if (address.houseNumber) {
-          result = result + ' ' + address.houseNumber;
-        }
-        if (address.apartmentNumber) {
-          result = result + ' / ' + address.apartmentNumber;
-        }
-
-        return result;
-      };
-    })
-
-
-    .run(function ($rootScope, $http, $compile, $timeout, $location) {
-      // set page title
-      $rootScope.setTitle = function(title, scope) {
-        if (scope) {
-          // assignment does not work if it changes another compiled part
-          $rootScope.title = '';
-          $timeout(function() {
-            $rootScope.title = $compile(title)(scope).get();
-          });
-        } else {
-          $rootScope.title = title;
-        }
-      };
-
-      // default date configuration
-      $rootScope.dateOptions = {
-        appendToBody: true//,
-        //dateFormat: 'yy-mm-dd'
-      };
-
-      /**
-       * Actual year value.
-       */
-      $rootScope.actualYear = moment().format('YYYY');
-
-      /**
-       * Printing file. Appends iframe with url to file.
-       */
-      $rootScope.printFile = function (urlOfFile) {
-        var path = $location.protocol() + '://' + $location.host() + ':' + $location.port() + urlOfFile;
-        // print
-        var iframe = document.createElement('iframe');
-        iframe.src = path;
-        iframe.style.display = 'none';
-        var iFrameLoaded = function() {
-          iframe.contentWindow.print();
-          iframe.parentNode.removeChild(iframe);
+      .run(function ($rootScope, $injector) {
+        $rootScope.getService = function (name) {
+          return $injector.get(name);
         };
-        if (iframe.attachEvent) { 
-          iframe.attachEvent('onload', iFrameLoaded); // for IE
-        } else if (iframe.addEventListener) {
-          iframe.addEventListener('load', iFrameLoaded, false); // for most other browsers
-        } else {
-          iframe.onload = iFrameLoaded; // just in case there's a browser not covered by the first two
-        }
-
-        document.body.appendChild(iframe);
-      };
-    })
+        $rootScope.hasService = function (name) {
+          return $injector.has(name);
+        };
+      })
 
 
-    // translation configuration
-
-    .config(function ($translateProvider) {
-      // tell angular-translate to use your custom handler
-      $translateProvider.useMissingTranslationHandler('mtTranslateHandlerFactory');
-    })
-
-    .run(function ($rootScope, $translate) {
-      // provide $translate service to all scopes
-      $rootScope.translate = $translate.instant;
-    })
-
-  // define custom handler
-    /**
-     * @ngdoc function
-     * @name mt.ui.factory:mtTranslateHandlerFactory
-     * @description
-     * # mtTranslateHandlerFactory
-     * Factory of the mt.ui
-     */
-    .factory('mtTranslateHandlerFactory', function ($translate) {
-      // has to return a function which gets a tranlation ID
-      return function (translationID) {
-        if (translationID.lastIndexOf('.') > 0) {
-          var translation = $translate.instant(translationID.substr(translationID.lastIndexOf('.') + 1));
-          return _.endsWith(translationID, translation) ? translationID : translation;
-        } else {
-          return translationID;
-        }
-      };
-    })
-    .config(function (restFactoryProvider) {
-      restFactoryProvider.define('Login', {
-        urlEntityName: 'login',
-        additionalFunctions: {
-          getUser: { url: '/user' },
-          logoff: { url: '/logoff', method: 'POST' },
-          login: {
-            method: 'POST'
+      .filter('addressPresenter', function() {
+        return function (address) {
+          if (!address) {
+            return;
           }
-        }
-      });
-    })
-    .provider('userGroupConfiguration', function (mtRouteConfig) {
-      var self = this;
+          var result = '';
 
-      self.groupToDefaultRoute = [];
+          if (address.zipCode) {
+            result = result + address.zipCode + ' ';
+          }
+          result = result + address.town + '<br/>';
+          result = result + address.street;
+          if (address.houseNumber) {
+            result = result + ' ' + address.houseNumber;
+          }
+          if (address.apartmentNumber) {
+            result = result + ' / ' + address.apartmentNumber;
+          }
 
-      this.$get = function ($rootScope, $route) {
-        return {
-          // placeholder for refreshing user group configuration
-          refreshAppUserConfiguration: function () {
-            var groups = _.pluck($rootScope.user.groups, 'name');
-            // default group
-            groups.push('');
-            var element = _.find(self.groupToDefaultRoute, function (element) {
-              return _.contains(groups, element.groupName);
+          return result;
+        };
+      })
+
+
+      .run(function ($rootScope, $http, $compile, $timeout, $location) {
+        // set page title
+        $rootScope.setTitle = function(title, scope) {
+          if (scope) {
+            // assignment does not work if it changes another compiled part
+            $rootScope.title = '';
+            $timeout(function() {
+              $rootScope.title = $compile(title)(scope).get();
             });
-
-            mtRouteConfig.defaultRoute = element ? element.defaultRoute : '';
-            $route.routes['null'] = mtRouteConfig.defaultRoute;
+          } else {
+            $rootScope.title = title;
           }
         };
-      };
 
-      return self;
-    })
-    /**
-     * @ngdoc function
-     * @name mt.ui.controller:HelpModalCtrl
-     * @description
-     * # HelpModalCtrl
-     * Controller of the mt.ui
-     */
-    .controller('HelpModalCtrl', function ($scope, $modalInstance) {
-      $scope.close = function(result) { $modalInstance.close(result); };
-    });
+        // default date configuration
+        $rootScope.dateOptions = {
+          appendToBody: true//,
+          //dateFormat: 'yy-mm-dd'
+        };
+
+        /**
+         * Actual year value.
+         */
+        $rootScope.actualYear = moment().format('YYYY');
+
+        /**
+         * Printing file. Appends iframe with url to file.
+         */
+        $rootScope.printFile = function (urlOfFile) {
+          var path = $location.protocol() + '://' + $location.host() + ':' + $location.port() + urlOfFile;
+          // print
+          var iframe = document.createElement('iframe');
+          iframe.src = path;
+          iframe.style.display = 'none';
+          var iFrameLoaded = function() {
+            iframe.contentWindow.print();
+            iframe.parentNode.removeChild(iframe);
+          };
+          if (iframe.attachEvent) {
+            iframe.attachEvent('onload', iFrameLoaded); // for IE
+          } else if (iframe.addEventListener) {
+            iframe.addEventListener('load', iFrameLoaded, false); // for most other browsers
+          } else {
+            iframe.onload = iFrameLoaded; // just in case there's a browser not covered by the first two
+          }
+
+          document.body.appendChild(iframe);
+        };
+      })
+
+
+      // translation configuration
+
+      .config(function ($translateProvider) {
+        // tell angular-translate to use your custom handler
+        $translateProvider.useMissingTranslationHandler('mtTranslateHandlerFactory');
+      })
+
+      .run(function ($rootScope, $translate) {
+        // provide $translate service to all scopes
+        $rootScope.translate = $translate.instant;
+      })
+
+      // define custom handler
+      /**
+       * @ngdoc function
+       * @name mt.ui.factory:mtTranslateHandlerFactory
+       * @description
+       * # mtTranslateHandlerFactory
+       * Factory of the mt.ui
+       */
+      .factory('mtTranslateHandlerFactory', function ($translate) {
+        // has to return a function which gets a tranlation ID
+        return function (translationID) {
+          if (translationID.lastIndexOf('.') > 0) {
+            var translation = $translate.instant(translationID.substr(translationID.lastIndexOf('.') + 1));
+            return _.endsWith(translationID, translation) ? translationID : translation;
+          } else {
+            return translationID;
+          }
+        };
+      })
+      .config(function (restFactoryProvider) {
+        restFactoryProvider.define('Login', {
+          urlEntityName: 'login',
+          additionalFunctions: {
+            getUser: { url: '/user' },
+            logoff: { url: '/logoff', method: 'POST' },
+            login: {
+              method: 'POST'
+            }
+          }
+        });
+      })
+      .provider('userGroupConfiguration', function (mtRouteConfig) {
+        var self = this;
+
+        self.groupToDefaultRoute = [];
+
+        this.$get = function ($rootScope, $route) {
+          return {
+            // placeholder for refreshing user group configuration
+            refreshAppUserConfiguration: function () {
+              var groups = _.pluck($rootScope.user.groups, 'name');
+              // default group
+              groups.push('');
+              var element = _.find(self.groupToDefaultRoute, function (element) {
+                return _.contains(groups, element.groupName);
+              });
+
+              mtRouteConfig.defaultRoute = element ? element.defaultRoute : '';
+              $route.routes['null'] = mtRouteConfig.defaultRoute;
+            }
+          };
+        };
+
+        return self;
+      })
+      /**
+       * @ngdoc function
+       * @name mt.ui.controller:HelpModalCtrl
+       * @description
+       * # HelpModalCtrl
+       * Controller of the mt.ui
+       */
+      .controller('HelpModalCtrl', function ($scope, $modalInstance) {
+        $scope.close = function(result) { $modalInstance.close(result); };
+      });
+
+
+  function dateFormatService () {
+    return function (format) {
+      return function (input) {
+        return input ? parseDate(input).format(format) : '';
+      };
+    };
+  }
+
+  // for each key register date filter with that name and value as format
+  angular.forEach({
+      'formatDate': 'YYYY-MM-DD',
+      'formatTime': 'HH:mm',
+      'formatDateTime': 'YYYY-MM-DD HH:mm',
+      'formatDayMoment': 'ddd HH:mm',
+      'weekdayLabel': 'dddd'
+  }, function (value, key) {
+    mt_ui.filter(key, function () { return dateFormatService(value); });
+  });
+})();
 
 /**
  * For unstable directives.
