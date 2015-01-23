4841e633639db05fcaf5aed169a310c288e94365
Marek Romanowski <marek.romanowski@gmail.com>
2015-01-23 20:22:51 +0100

    Fix test

--- test/scripts/spec/directives/simpleSearch.js
+++ test/scripts/spec/directives/simpleSearch.js
@@ -6,15 +6,14 @@ describe('Simple search', function () {
     scope.$digest();
     scope.$digest();
     var templateAsHtml = template.html();
-    expect(templateAsHtml).toBe('<form class="form-inline simple-search-form ng-isolate-scope ng-pristine ng-valid" ' +
+    expect(templateAsHtml).toBe('<form class="form-inline simple-search-form ng-pristine ng-valid ng-isolate-scope" ' +
         'simple-search="" placeholder="test"><div class="input-group"><input type="text" name="query" ng-model="query" ' +
         'class="form-control ng-pristine ng-untouched ng-valid" placeholder="test"><span ng-show="$parent.__loading" ' +
         'class="fa fa-spinner fa-spin form-control-feedback ng-hide"></span><div class="input-group-btn"><button ' +
         'type="button" ng-click="clearQuery()" class="btn btn-default"><i class="fa fa-times"></i></button>' +
         '<!-- ngIf: showSearchButton --></div></div><span ng-transclude=""></span></form>');
   });
-// '<form class="form-inline simple-search-form ng-pristine ng-valid ng-isolate-scope" simple-search="" placeholder="test"><div class="input-group"><input type="text" name="query" ng-model="query" class="form-control ng-pristine ng-untouched ng-valid" placeholder="test"><span ng-show="$parent.__loading" class="fa fa-spinner fa-spin form-control-feedback ng-hide"></span><div class="input-group-btn"><button type="button" ng-click="clearQuery()" class="btn btn-default"><i class="fa fa-times"></i></button><!-- ngIf: showSearchButton --></div></div><span ng-transclude=""></span></form>'
-// '<form class="form-inline simple-search-form ng-isolate-scope ng-pristine ng-valid" simple-search="" placeholder="test"><div class="input-group"><input type="text" name="query" ng-model="query" class="form-control ng-pristine ng-untouched ng-valid" placeholder="test"><span ng-show="$parent.__loading" class="fa fa-spinner fa-spin form-control-feedback ng-hide"></span><div class="input-group-btn"><button type="button" ng-click="clearQuery()" class="btn btn-default"><i class="fa fa-times"></i></button><!-- ngIf: showSearchButton --></div></div><span ng-transclude=""></span></form>'
+
 
   it('clears search query', function () {
     scope.query = 'fdfsdf';
